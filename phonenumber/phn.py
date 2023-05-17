a334711eccd6c0ba7cbc1bda1f0c71d2da3ebbdc
SUMANTH <127339877+sumanthgubbala@users.noreply.github.com>
2023-05-17 14:21:37 +0530

    Add files via upload

---
+++ phonenumber/phn.py
@@ -0,0 +1,9 @@
+import phonenumbers
+from test import number 
+
+from phonenumbers import geocoder
+from phonenumbers import carrier
+phn =phonenumbers.parse(number, "CH")
+print(geocoder.description_for_number(phn,"en"))
+ser =phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(ser,"en"))
